b44431d2deeb0f2e1682c0795ef487e62d1a80d0
Nhukangasniemi <quynhnhu.ho2310@gmail.com>
2021-10-06 18:53:13 +0300

    Adding Header

--- packages/container/src/App.tsx
+++ packages/container/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
 
 export default () => {
   return (
-    <div>
-      <h1>Hi there!!!!</h1>
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header signedIn={true} onSignOut={() => console.log('Hello')} />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
